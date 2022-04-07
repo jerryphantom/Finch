c90d1574c68d1b89c16374b02e995fccc3866777
Jeremiah Butler <jeremiah@matthews-MBP.hsd1.ca.comcast.net>
2022-04-06 18:23:19 -0700

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,20 @@
+from BirdBrain import Finch
+bird = Finch()
+print(bird.getDistance())
+
+def exercise1():
+    print("Distance: ", bird.getDistance())
+    
+def exercise2():
+    print("get right Light: ", bird.getLight("R"))
+    print("get left Light:",bird.getLight("L"))
+    print("get button :",bird.getButton("A"))
+    print("get Orientation:",bird.getOrientation())
+    print("getEncoder:",bird.getEncoder("R"))
+    
+def exercise3():
+    print(type(birdgetDistince()))
+    
+exercise1()
+exercise2()
+exercise3()
